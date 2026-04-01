55ce2369bf80d771733d76ff6f585b74ec04ff8d
Захар Проваторов <pzv1806@gmail.com>
2026-04-01 19:43:57 +0300

    add blue rectangle that follows the right mouse button

--- less1_game.py
+++ less1_game.py
@@ -17,6 +17,8 @@ dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 target_pos_x, target_pos_y = bullet_pos_x, bullet_pos_y = -100, -100
+object_rect = pygame.Rect((screen.get_width() / 2-100, screen.get_height() / 2-100, 40, 40))
+target2_pos_x, target2_pos_y = object_rect.x,object_rect.y
 
 while running:
     screen.fill((0,0,0))
@@ -27,7 +29,10 @@ while running:
             if event.button == 2:
                 target_pos_x, target_pos_y = pygame.mouse.get_pos()
                 bullet_pos_x, bullet_pos_y = player_pos
+            if event.button == 3:
+                target2_pos_x, target2_pos_y = pygame.mouse.get_pos()
     pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.rect(screen, "blue", object_rect)
 
     key = pygame.key.get_pressed()
     if key[pygame.K_w]:
@@ -55,6 +60,22 @@ while running:
         
     if bullet_pos_y in range(int(target_pos_y)-10,int(target_pos_y)+10):
         bullet_pos_y = target_pos_y
+    
+    if object_rect.x < target2_pos_x-20:
+        object_rect.x += 3
+    elif object_rect.x > target2_pos_x-20:
+        object_rect.x -= 3
+        
+    if object_rect.x in range(int(target_pos_x-20)-10,int(target_pos_y-20)+10):
+        object_rect.x = target2_pos_x
+        
+    if object_rect.y < target2_pos_y-20:
+        object_rect.y += 3
+    elif object_rect.y > target2_pos_y-20:
+        object_rect.y -= 3
+        
+    if object_rect.y in range(int(target2_pos_y-20)-10,int(target2_pos_y-20)+10):
+        object_rect.y = target2_pos_y-20
         
     if (target_pos_x, target_pos_y) != (bullet_pos_x, bullet_pos_y):
         pygame.draw.circle(screen, "green", (bullet_pos_x, bullet_pos_y), 20)
