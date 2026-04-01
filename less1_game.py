253e03744f1a5b50b289d56d9a891a4eec46df20
Захар Проваторов <pzv1806@gmail.com>
2026-04-01 19:51:41 +0300

    add yellow ellipse that can be replaced by left mouse button

--- less1_game.py
+++ less1_game.py
@@ -17,8 +17,9 @@ dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 target_pos_x, target_pos_y = bullet_pos_x, bullet_pos_y = -100, -100
-object_rect = pygame.Rect((screen.get_width() / 2-100, screen.get_height() / 2-100, 40, 40))
+object_rect = pygame.Rect((screen.get_width() / 2-100, screen.get_height() / 2-100, 60, 40))
 target2_pos_x, target2_pos_y = object_rect.x,object_rect.y
+moving=False
 
 while running:
     screen.fill((0,0,0))
@@ -26,13 +27,23 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                target2_pos_x, target2_pos_y = pygame.mouse.get_pos()
+                if target2_pos_x in range(int(object_rect.x-60),int(object_rect.x+60)) and target2_pos_y in range(int(object_rect.y-40),int(object_rect.y+40)):
+                    moving = True
             if event.button == 2:
                 target_pos_x, target_pos_y = pygame.mouse.get_pos()
                 bullet_pos_x, bullet_pos_y = player_pos
-            if event.button == 3:
+            if event.button == 1:
                 target2_pos_x, target2_pos_y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEMOTION:
+                if moving:
+                    x_new, y_new = event.rel
+                    object_rect.x, object_rect.y = object_rect.x + x_new, object_rect.y + y_new
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                moving = False
     pygame.draw.circle(screen, "red", player_pos, 40)
-    pygame.draw.rect(screen, "blue", object_rect)
+    pygame.draw.ellipse(screen, "yellow", object_rect)
 
     key = pygame.key.get_pressed()
     if key[pygame.K_w]:
@@ -60,22 +71,6 @@ while running:
         
     if bullet_pos_y in range(int(target_pos_y)-10,int(target_pos_y)+10):
         bullet_pos_y = target_pos_y
-    
-    if object_rect.x < target2_pos_x-20:
-        object_rect.x += 3
-    elif object_rect.x > target2_pos_x-20:
-        object_rect.x -= 3
-        
-    if object_rect.x in range(int(target_pos_x-20)-10,int(target_pos_y-20)+10):
-        object_rect.x = target2_pos_x
-        
-    if object_rect.y < target2_pos_y-20:
-        object_rect.y += 3
-    elif object_rect.y > target2_pos_y-20:
-        object_rect.y -= 3
-        
-    if object_rect.y in range(int(target2_pos_y-20)-10,int(target2_pos_y-20)+10):
-        object_rect.y = target2_pos_y-20
         
     if (target_pos_x, target_pos_y) != (bullet_pos_x, bullet_pos_y):
         pygame.draw.circle(screen, "green", (bullet_pos_x, bullet_pos_y), 20)
